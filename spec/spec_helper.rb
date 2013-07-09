543b289f8847b3f7e335ae5c152c3f105eedfc08
stasialopatina <stasialopatina@gmail.com>
2013-07-09 12:10:46 +0400

    Add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 require 'rspec'
 require 'hashie'
 
+
+if ENV['TRAVIS']
+  require 'coveralls'
+  Coveralls.wear!
+end
+
 RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
