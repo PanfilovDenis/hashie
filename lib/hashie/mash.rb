c2ce3917dfc93e93fa7486a7800d37eeb07e3763
PanfilovDenis <panf.denis@gmail.com>
2013-07-09 09:57:53 +0000

    add part of mash

--- lib/hashie/mash.rb
+++ lib/hashie/mash.rb
@@ -1,5 +1,28 @@
 module Hashie
-  class Mash < Hash.new 
-    
+  class Mash < Hash
+    def method_missing(method_name, *args, &block)
+      
+      type = method_name[-1]
+      method = method_name.to_s.chomp("=").to_sym
+
+      case type
+      when "="
+        self[method] = args.first
+        self.define_singleton_method("#{method}=") do
+          args.first
+        end
+      when "?"
+      else
+        self.define_singleton_method(method) do
+          self[method]
+        end
+      end
+    end
+
+    def respond_to_missing?(name, include_private = false)
+      true
+    end
+
+
   end
 end
--- spec/hashie_spec.rb
+++ spec/hashie_spec.rb
@@ -1,7 +1,26 @@
 require 'spec_helper'
 
 describe Hashie do
-  it 'should return true' do
-    1 == 1
+
+  before(:each) do
+    @mash = Hashie::Mash.new
   end
+
+  # it 'should be nil on undefined key' do
+  #   @mash.undefined
+  #   # @mash.undefined.should be_nil
+  # end
+
+  it 'should be get nil if undefined key' do
+    expect(nil).to eq(@mash.undefined)
+  end
+
+
+  it 'should be set and get value' do
+    value = "test"
+    @mash.defined = value
+    expect(value).to eq(@mash.defined)
+  end
+
+
 end
