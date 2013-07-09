9e9a9ba868849ca455277fb2ee2e180f6b7cceaa
PanfilovDenis <panf.denis@gmail.com>
2013-07-09 10:11:51 +0000

    add mash

--- lib/hashie/mash.rb
+++ lib/hashie/mash.rb
@@ -3,26 +3,28 @@ module Hashie
     def method_missing(method_name, *args, &block)
       
       type = method_name[-1]
-      method = method_name.to_s.chomp("=").to_sym
+      method = method_name.to_s.chomp("=").chomp("?").to_sym
 
       case type
       when "="
         self[method] = args.first
-        self.define_singleton_method("#{method}=") do
+        self.define_singleton_method(method) do
           args.first
         end
       when "?"
-      else
-        self.define_singleton_method(method) do
-          self[method]
+        self.define_singleton_method("#{method}?") do
+          self.key?(method)
         end
+        self.key?(method)
+      else
+        nil
       end
+
     end
 
     def respond_to_missing?(name, include_private = false)
       true
     end
-
 
   end
 end
--- spec/hashie_spec.rb
+++ spec/hashie_spec.rb
@@ -6,15 +6,9 @@ describe Hashie do
     @mash = Hashie::Mash.new
   end
 
-  # it 'should be nil on undefined key' do
-  #   @mash.undefined
-  #   # @mash.undefined.should be_nil
-  # end
-
   it 'should be get nil if undefined key' do
     expect(nil).to eq(@mash.undefined)
   end
-
 
   it 'should be set and get value' do
     value = "test"
@@ -22,5 +16,14 @@ describe Hashie do
     expect(value).to eq(@mash.defined)
   end
 
+  it 'should be get false if undefined key' do
+    expect(false).to eq(@mash.undefined?)
+  end
+
+  it 'should be get true if defined key' do
+    value = "test"
+    @mash.defined = value
+    expect(true).to eq(@mash.defined?)
+  end
 
 end
