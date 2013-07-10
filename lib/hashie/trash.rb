d849a1020f400ee15a12a2954abe4f8d7f801ce3
stasialopatina <stasialopatina@gmail.com>
2013-07-10 11:07:17 +0200

    Add Trash

--- lib/hashie/trash.rb
+++ lib/hashie/trash.rb
@@ -5,21 +5,33 @@ module Hashie
       super method_name, params
 
       @aliases ||= {}
+      @lambdas ||= {}
 
       if params.key?(:from)
-         @aliases[method_name] = params[:from]
+        @aliases[method_name] = params[:from]
       end
 
+      if params.key?(:transform_with)
+        @lambdas[method_name] = params[:transform_with]
+      end
     end
 
     def self.aliases
       @aliases
     end
 
+    def self.lambdas
+      @lambdas
+    end
+
     def initialize(args)
+      super
       self.class.aliases.each do |key, value|
         self[key] = args[value]
       end
+      puts "123"
+      p args
+
     end
 
   end
--- spec/lib/trash_spec.rb
+++ spec/lib/trash_spec.rb
@@ -3,11 +3,19 @@ require 'spec_helper'
 describe Hashie::Trash do
   before(:each) do
   	@first_name = "Ivan"
-    @man = Man.new(firstName: @first_name)
+  	@id = "1234"
+  	@creation_date = "2012-03-30 17:23:28"
+    @man = Man.new(firstName: @first_name, id: @id, creation_date: @creation_date)
+    p @man
   end
 
    it 'should be get exist name' do
-    expect(@first_name).to eq(@man.first_name)
-  end
+     expect(@first_name).to eq(@man.first_name)
+   end
+   
+   it 'should be get Fixnum' do
+     expect(Fixnum).to eq(@man.id)
+   end
+
 
 end
